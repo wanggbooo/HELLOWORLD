44891aee516bda8ef460f18ceb8819d238104547
wanggbooo <939071086@qq.com>
2015-07-09 21:12:45 +0800

    Create dhf1.php

---
+++ dhf1.php
@@ -0,0 +1,21 @@
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+        <title>dhf1</title>
+
+  
+    
+</head>
+<body>
+    <a href="http://www.imooc.com"target="view_window">慕课网</a><br />
+
+     <a href="http://www.w3school.com.cn"target="view_window">w3school</a><br />
+
+     <a href="http://sae.sina.com.cn"target="view_window">sea</a><br />
+
+     <a href="http://www.imooc.com"target="view_window">慕课网</a><br />
+
+     <a href="http://www.imooc.com"target="view_window">慕课网</a><br />
+
+</body>
+</html>
